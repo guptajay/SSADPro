8422b6eebc9227e2e2f0ce1a4d1e485a3a37cab4
Jay Gupta <jaygupta2607@gmail.com>
2020-03-13 15:58:58 +0800

    Application Skeleton

---
+++ ssad_pro/lib/main.dart
@@ -0,0 +1,21 @@
+/**
+ * This is the controller class which initialises the application.
+ *
+ * @author XYZ
+ */
+import 'package:flutter/material.dart';
+import 'package:ssadpro/view/login.dart';
+
+void main() => runApp(Startup());
+
+class Startup extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: true,
+        title: "SSADPro",
+        home: Login()
+    );
+  }
+}
