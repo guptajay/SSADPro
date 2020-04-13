c145f8ac353a94c4316ffd7f91f280bf69503ccb
Jay Gupta <jay002@e.ntu.edu.sg>
2020-04-13 23:01:58 +0800

    Small Bug Fix

--- ssad_pro/lib/main.dart
+++ ssad_pro/lib/main.dart
@@ -35,7 +35,7 @@ class Startup extends StatelessWidget {
               textTheme: Theme.of(context).textTheme.apply(
                     fontFamily: 'Open Sans',
                   )),
-          home: LevelViewPage()),
+          home: Wrapper()),
     );
   }
 }
